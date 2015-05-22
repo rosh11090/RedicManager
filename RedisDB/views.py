ca19b5282eede06b1ef5338dd765781f838179dc
Roshan Patel <roshan.patel@goibibo.com>
2015-05-22 19:55:06 +0530

    new project added

---
+++ RedisDB/views.py
@@ -0,0 +1,13 @@
+import redis
+
+
+class RedisMenager():
+    """
+    Class to MAnage multiple Redis.
+    """
+    def __init__(self, *args, **kwrgs):
+        self.noOfRedis = 0
+        pass
+
+    def getRedis(key):
+        x = reduce()
